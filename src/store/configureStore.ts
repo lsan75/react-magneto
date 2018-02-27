7fd30195d72d29a32cb7e4183fed8873d88ad561
Laurent Sanchez <laurentsanchez@macbook-pro-de-laurent-2.home>
2018-02-28 00:18:24 +0100

    add redux dev tools

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,13 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { rootReducer } from './rootReducer';
 import { defaulStore } from './root';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore() {
   return createStore(
     rootReducer,
     defaulStore,
-    applyMiddleware(
+    composeWithDevTools(applyMiddleware(
       thunkMiddleware
-    )
+    ))
   );
 }
