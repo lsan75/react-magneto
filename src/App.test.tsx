a01ee50172d488d26cbd58ed5e67b81aea9e0ae4
Laurent Sanchez <lsanchez.ext@galerieslafayette.com>
2018-03-15 16:40:12 +0100

    fix app unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
+  , div);
 });
